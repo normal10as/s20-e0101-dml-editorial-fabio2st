7c35e56c324c95ad804c22cbaff92dd7bd0791cc
Fabio Dos Santos <fabio2st@yahoo.com>
2020-04-09 23:36:59 -0300

    Create Select all tables.sql
    Primer envío

---
+++ Select all tables.sql
@@ -0,0 +1,21 @@
+SELECT * FROM almacenes
+
+SELECT * FROM autores
+
+SELECT * FROM cargos
+
+SELECT * FROM descuentos
+
+SELECT * FROM editorial_info -- check
+
+SELECT * FROM editoriales
+
+SELECT * FROM empleados
+
+SELECT * FROM plan_regalias
+
+SELECT * FROM titulo_autor
+
+SELECT * FROM titulos
+
+SELECT * FROM ventas
